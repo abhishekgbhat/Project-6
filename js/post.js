e9d1416a68c84805192e490a03d6750c169f59b8
ishita169 <ishuarials@gmail.com>
2019-04-07 23:43:37 +0530

    Changes made in post.js

--- js/post.js
+++ js/post.js
@@ -4,9 +4,10 @@ var likesOfPeople = document.getElementById("p2");
 var editIcon = document.getElementById("editIcon");
 var saveIcon = document.getElementById("saveIcon");
 var editableParagraph = document.getElementById("blogBody");
+var editableBlogTitleNew =document.getElementById("blogTitleNew");
 var commentTemplate = document.getElementById("addComment");
 var commentText = "";
-
+/*Count Like */
 function likeFunction(){
     noOFLikes++;
     if(noOFLikes==1) {
@@ -18,22 +19,31 @@ function likeFunction(){
         likesOfPeople.innerHTML = noOFLikes+ " people have liked this!";
     }
 }
-
+/*Edit event*/
 function editAndSave() {
-    if(editableParagraph.contentEditable == "false"){
+    if(editableParagraph.contentEditable == "false" && editableBlogTitleNew.contentEditable == "false"){
         editableParagraph.contentEditable = "true";
         saveIcon.style.display = "block";
         editIcon.style.display = "none";
-        editableParagraph.style.border ="1px solid grey";
+        editableParagraph.style.border ="1px solid grey";/*Pragraph Edit*/
+		
+		editableBlogTitleNew.contentEditable ="true";
+		editableBlogTitleNew.style.border="2px solid grey";
     }
     else{
         editableParagraph.contentEditable = "false";
         saveIcon.style.display = "none";
         editIcon.style.display = "block";
         editableParagraph.style.border ="none";
+		
+		editableBlogTitleNew.contentEditable = "false";
+		editableBlogTitleNew.style.border="none";
+		
         editableParagraph.defaultValue += editableParagraph.value;
+		editableBlogTitleNew.defaultValue += editableBlogTitleNew.value;
     }
 }
+/*add and Display Comment*/
 function addComments(id) {
     if(id.value == "") {
         return;
