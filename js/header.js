3e44ab493b7bc694c6a10e58bbeaf053c8d3653e
Abhishek G Bhat <abhishek.bhat5@gmail.com>
2019-04-07 23:55:10 +0530

    Final changes with styling

--- js/header.js
+++ js/header.js
@@ -2,36 +2,33 @@ var signIn = document.getElementById("SignInModal");
 var signUp = document.getElementById("SignUpModal");
 var createPost = document.getElementById("createPostModal");
 
-function modalDialog1() {
+function modalDialog1 () {
     signIn.style.display = "block";
 }
 
-function hideModal1(){
+function hideModal1 () {
     signIn.style.display = "none";
 }
 
-function modalDialog2(){
+function modalDialog2 () {
     signUp.style.display = "block";
 }
 
-function hideModal2(){
+function hideModal2 () {
     signUp.style.display = "none";
 }
 
-document.onclick = function(event){
+document.onclick = function (event) {
     if (event.target == signIn) {
       signIn.style.display = "none";
-    }
-   else if(event.target == signUp)
-    {
+    } else if (event.target == signUp) {
         signUp.style.display = "none";
-    }
-    else if(event.target == createPost) {
+    } else if (event.target == createPost) {
         createPost.style.display = "none";
     }
   };
 
-  function newModal(){
-    signIn.style.display = "none";
-    signUp.style.display = "block";
-  }
+function newModal () {
+  signIn.style.display = "none";
+  signUp.style.display = "block";
+}
--- js/post.js
+++ js/post.js
@@ -7,19 +7,19 @@ var editableParagraph = document.getElementById("blogBody");
 var commentTemplate = document.getElementById("addComment");
 var commentText = "";
 
-function likeFunction(){
+function likeFunction () {
     noOFLikes++;
-    if(noOFLikes==1) {
+    if (noOFLikes==1) {
         like.innerHTML = "<i class='fa fa-thumbs-up'></i>Liked!";
         likesOfPeople.innerHTML = "1 person likes this!";
     }
-    else{
+    else {
         like.innerHTML = "<i class='fa fa-thumbs-up'></i>Liked!";
-        likesOfPeople.innerHTML = noOFLikes+ " people have liked this!";
+        likesOfPeople.innerHTML = noOFLikes + " people have liked this!";
     }
 }
 
-function editAndSave() {
+function editAndSave () {
   if (blogBody.contentEditable == 'true') {
       blogBody.contentEditable = 'false';
       blogBody.style.border = 'none';
@@ -28,19 +28,19 @@ function editAndSave() {
       button.innerHTML = 'Edit <i class="fa fa-edit"></i>';
   } else {
       blogBody.contentEditable = 'true';
-      blogBody.style.border = '1px solid red';
+      blogBody.style.border = '1px solid blue';
       blogTitleNew.contentEditable = 'true';
       blogTitleNew.style.border = '1px solid red';
       button.innerHTML = 'Save <i class="fa fa-save"></i>';
   }
 }
-function addComments(id) {
-    if(id.value == "") {
+
+function addComments (id) {
+    if (id.value == "") {
         return;
-    }
-    else{
-    commentText = id.value +'<br>';
-    commentTemplate.innerHTML += "<p class='p4'>"+commentText+"</p>";
-    id.value = id.defaultValue;
+    } else {
+      commentText = id.value +'<br>';
+      commentTemplate.innerHTML += "<p class='p4'>" + commentText + "</p>";
+      id.value = id.defaultValue;
     }
 }
